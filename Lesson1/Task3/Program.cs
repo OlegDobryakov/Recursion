50984d61516fabe14aeffd3a824ea552449a3331
Oleg Dobryakov <komhin3649@gmal.com>
2024-01-30 17:47:45 +0300

    сравнение

--- Lesson1/Task3/Program.cs
+++ Lesson1/Task3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 8;
+
+if (a > b)
+{
+    Console.WriteLine (a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+

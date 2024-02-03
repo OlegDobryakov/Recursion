cf700f1bc42dfd9e27aeed3dee3d16a44a1a8bad
Oleg Dobryakov <komhin3649@gmal.com>
2024-02-03 18:20:25 +0300

    lesson2:task4(2)

--- Lesson2/Task4/Program.cs
+++ Lesson2/Task4/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int n = 10;
+int[] array = { 2, 3, 9, 5, 1, 12, 73, 23, 64, 62 };
+int i = 0;
+int max = 0;
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+      max = array[i];
+    }  
+  
+  i = i + 1;
+}
+Console.Write($"{max} ");

52b1d5bc5375a98747d4bfbb0dcad3a33e1ba351
Oleg Dobryakov <komhin3649@gmal.com>
2024-02-03 18:11:20 +0300

    lesson2:task3(2)

--- Lesson2/Task3/Program.cs
+++ Lesson2/Task3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 10;
+int[] array = { 2, 3, 9, 5, 1, 12, 73, 23, 64, 62 };
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+      Console.Write($"{array[i]} ");
+    }  
+  
+  i = i + 1;
+}

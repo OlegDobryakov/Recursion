279c86dae3ef0482d5c2b033d6d907829bf989c7
Oleg Dobryakov <komhin3649@gmal.com>
2024-02-06 11:45:21 +0300

    completed ar4

---
+++ Arrays/arr4/ar4/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] array = { 1, 6, 3, 10, 4, 9, 5, 5, 9, 5, 7, 939, 5, 21};
+int i = 0;
+int size = 0;
+int j = 0;
+int n = 0;
+while (j < array.Length)
+{
+    j++;
+}
+j--;
+while (i < j)
+{
+    array[n] = array[i] * array[j];
+    n++;
+    i++;
+    j--;
+    size++;
+
+    if (i == j)
+    {
+        n++;
+        Console.WriteLine($"Нету {n} пары.");
+    }
+
+}
+for (n = 0; n < size; n++)
+{
+    Console.Write(array[n] + " ");
+}
+

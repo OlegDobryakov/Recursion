bba7906aa0788f74615bdaebd84448dcfa077597
Oleg Dobryakov <komhin3649@gmal.com>
2024-02-06 11:47:39 +0300

    completed ar2

---
+++ Arrays/arr2/ar2/Program.cs
@@ -0,0 +1,41 @@
+﻿
+int[] array =  {5, 9, 72, 8};
+int a;
+Console.WriteLine("Введите число для проверки: ");
+a = int.Parse(Console.ReadLine()!);
+int b = 0;
+for (int i = 0; i < array.Length; i++)
+{  
+   if (a == array[i])
+   { b = 9;
+    Console.WriteLine("Да");
+   }
+
+}
+if (b == 0)
+{
+    Console.WriteLine("Нет");
+}
+
+
+int[] array1 =  {5, 9, 72, 8};
+int a1;
+Console.WriteLine("Введите число для проверки: ");
+a1 = int.Parse(Console.ReadLine()!);
+bool result = false;
+for (int i = 0; i < array.Length; i++)
+{  
+   if (a1 == array1[i])
+   { 
+    result = true;
+   }
+
+}
+if (result == true)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+} 

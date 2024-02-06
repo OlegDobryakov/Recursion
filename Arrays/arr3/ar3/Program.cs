4c01fb98940efff311088ee20ef2251c5b6f9a1c
Oleg Dobryakov <komhin3649@gmal.com>
2024-02-06 09:25:10 +0300

    completed ar3

---
+++ Arrays/arr3/ar3/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = {-1, 6, 3, -10, -4, 9};
+int size = 6;
+for (int i = 0; i < size; i++)
+{
+    array[i] = array[i] * -1;
+}
+for (int i = 0; i < size; i++)
+{
+   Console.Write(array[i] + " ");
+}
+

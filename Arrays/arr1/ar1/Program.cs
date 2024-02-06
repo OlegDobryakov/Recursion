454d82da759e4ad963473674e2c1dcc34398d6e0
Oleg Dobryakov <komhin3649@gmal.com>
2024-02-06 11:47:19 +0300

    completed ar1

---
+++ Arrays/arr1/ar1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите размер массива: ");
+//int[] numbers = { 5, 123, 12312312 };
+int size = int.Parse(Console.ReadLine()!);
+int[] numbers = new int[size];
+
+Console.WriteLine($"Введите {size} элементов массива: ");
+for (int i = 0; i < size; i++)
+{
+    int input = int.Parse(Console.ReadLine()!);
+    numbers[i] = input;
+}
+
+Console.WriteLine("Массив: ");
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}

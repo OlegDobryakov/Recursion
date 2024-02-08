1f6b80bb64d854eb049f02a3b138b1cd416a22b6
Oleg Dobryakov <komhin3649@gmal.com>
2024-02-08 15:42:34 +0300

    lesson3:f1

---
+++ Lesson3/F1/f1/Program.cs
@@ -0,0 +1,9 @@
+﻿double Calculareformula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denonumenator = c + d;
+    double result = numenator / denonumenator;
+    return result;
+}
+double result = Calculareformula(1, 2, 3, 4);
+Console.WriteLine(result);

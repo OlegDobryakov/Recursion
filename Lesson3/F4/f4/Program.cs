7f098752ba5b675e4c36fac425a5cd039cb9a8ad
Oleg Dobryakov <komhin3649@gmal.com>
2024-02-10 13:35:44 +0300

    lesson3:f4

---
+++ Lesson3/F4/f4/Program.cs
@@ -0,0 +1,15 @@
+﻿int FindPrimeNumbers(int[] arr)
+{ int a = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 > 0)
+        {
+            a = a + 1;
+        }
+    } 
+    int result = a;
+    return result;
+}
+int[] array = [2, 6, 3, 7, 2, 19, 53, 23, 54, 724924, 13];
+int result = FindPrimeNumbers(array);
+Console.WriteLine(result);
